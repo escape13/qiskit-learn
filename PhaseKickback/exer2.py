6e180bd37f6e1319337a1e377f6dacf57f0e2227
Timur Valeev <valeevtim@gmail.com>
2020-12-22 12:24:30 +0300

    phase kickback exercises

---
+++ PhaseKickback/exer2.py
@@ -0,0 +1,15 @@
+from qiskit import Aer, execute, QuantumCircuit
+from qiskit.visualization import plot_bloch_multivector
+from math import pi
+import matplotlib.pyplot as plt
+
+qc = QuantumCircuit(2)
+qc.h(0)
+qc.x(1)
+qc.cu1(-pi/2, 0, 1)
+
+backend = Aer.get_backend('statevector_simulator')
+out = execute(qc, backend).result().get_statevector()
+print(out)
+plot_bloch_multivector(out)
+plt.show()
